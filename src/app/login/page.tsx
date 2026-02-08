b3259c1a1dbd7ae53df8d039203e95953e52a44c
WillisMcG <will_schulte@hotmail.com>
2026-02-08 13:00:53 -0600

    fix: escape apostrophe in login page for ESLint

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -238,7 +238,7 @@ export default function LoginPage() {
                 </Button>
 
                 <p className="text-center text-sm text-gray-500 dark:text-gray-400 mt-4">
-                  Don't have an account?{' '}
+                  Don&apos;t have an account?{' '}
                   <button
                     type="button"
                     onClick={() => {
